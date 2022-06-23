cd0ebea543f6b22a1ecd0adfdcb1a51ecd6625bb
YojZFK <vzikich@rambler.ru>
2022-06-23 23:19:01 +0300

    Makes mass statistic in simulations

--- src/com/yoj/collections/level2/part6/PasswordGenerator/SimulatorForPasswordGenerator.java
+++ src/com/yoj/collections/level2/part6/PasswordGenerator/SimulatorForPasswordGenerator.java
@@ -15,23 +15,50 @@ public class SimulatorForPasswordGenerator {
         ByteArrayOutputStream password = PasswordGenerator.generatePassword(lengthOfPassword);
         System.out.println();
         System.out.println(password.toString());
-
         Random randy = new Random();
-        for (int i = 20; i < 40; i++) {
-            password = PasswordGenerator.generatePassword(lengthOfPassword);
-          //  System.out.println();
-          // System.out.println(password.toString());
-            int simulatedRange = 15,  //randy.nextInt(i, i+20),
-                  //  simulatedPieces = randy.nextInt(1,simulatedRange / 3);
-                simulatedPieces = 14;
-            int[] simulatedResult = RangeRandomSplitter.getInstance().splitWithMinimalPiece(simulatedRange, simulatedPieces, 1);
-            // System.out.printf(" %B ", (Arrays.stream(simulatedResult).sum() == simulatedRange));
-            System.out.printf("Range: %d, pieces: %d, Split result: %s%n",simulatedRange, simulatedPieces, Arrays.toString(simulatedResult));
-        }
-
-
+        password = PasswordGenerator.generatePassword(lengthOfPassword);
+        testForRelativeMassOfElements();
     }
 
+    private static void show15SplitsWithMinimum1For(int range, int pieces){
+        System.out.println("\n");
+        for (int i = 0; i < 15; i++) {
+            int[] simulatedResult = RangeRandomSplitter.getInstance().splitWithMinimalPiece(range, pieces, 1);
+            System.out.printf("Range: %d, pieces: %d, Split result: %s%n", range, pieces, Arrays.toString(simulatedResult));
+        }
+    }
 
+    private static void testForRelativeMassOfElements(){
+        for (int i = 1; i < 6; i++) {
+            showRelativeElementsDistributionAverageFrom50SimulationsFor(i*20,19);
+        }
+        for (int i = 1; i < 6; i++) {
+            showRelativeElementsDistributionAverageFrom50SimulationsFor(30,i+1);
+        }
+    }
 
+    private static void showRelativeElementsDistributionAverageFrom50SimulationsFor(int range, int pieces){
+        int[] summaryOfSimulations = new int[pieces];
+        for (int i = 0; i < 50; i++) {
+            int[] singleSimulation = RangeRandomSplitter.getInstance().splitWithMinimalPiece(range, pieces, 1);
+            appendArraySourceTo(summaryOfSimulations, singleSimulation);
+
+        }
+        normalizeAndMultiplyBy100ElementsOf(summaryOfSimulations);
+        System.out.printf("Range: %d, pieces: %d, Split mass in %% of Range: %s%n", range, pieces, Arrays.toString(summaryOfSimulations));
+    }
+
+    private  static void appendArraySourceTo(int[] arrayMaster, int[] arraySource){
+        int i = 0;
+        for (int element: arraySource) {
+            arrayMaster[i++] += element;
+        }
+    }
+
+    private static void normalizeAndMultiplyBy100ElementsOf(int[] array){
+        int summaryOfElements = Arrays.stream(array).sum();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (array[i] * 100) / summaryOfElements ;
+        }
+    }
 }
