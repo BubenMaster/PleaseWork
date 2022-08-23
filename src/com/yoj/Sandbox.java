f244ce633829f780ae2776665570fedbee47c5c9
YojZFK <vzikich@rambler.ru>
2022-08-23 19:24:39 +0300

    Added jackson.databind lib

---
+++ src/com/yoj/Sandbox.java
@@ -0,0 +1,12 @@
+package com.yoj;
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("new line");
+        builder.append("\n");
+        builder.append("next new line");
+        System.out.println(builder.toString());
+    }
+}
